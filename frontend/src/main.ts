72cb02045769de893a390612058f4414a2d79b93
Caio Nunes <caionunes.profissional@gmail.com>
2025-04-07 22:10:04 -0300

    First Commit

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config';
 import Aura from '@primeuix/themes/aura';
-import Button from 'primevue/button';
+
 
 const app = createApp(App)
 app.use(router)
