d3e33e4de074e34f8ee09e8cd57fb3b6414f6f32
Yann Gabriel <yanngabriel@protonmail.com>
2025-05-15 19:28:01 -0300

    cleaning

--- backend/src/main/java/br/ucsal/youp/dto/AddHabilidadeRequest.java
+++ backend/src/main/java/br/ucsal/youp/dto/AddHabilidadeRequest.java
@@ -1,11 +1,1 @@
-package br.ucsal.youp.dto;
 
-import lombok.Data;
-
-@Data
-public class AddHabilidadeRequest {
-    private Long idFuncionario;
-    private Long idCargo;
-    private String nome;
-    private String tipo;
-}
