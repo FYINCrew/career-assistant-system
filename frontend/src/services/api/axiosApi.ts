0bd59827903c4f626237b2e375d0a4f822af8969
Caio Nunes <caionunes.profissional@gmail.com>
2025-06-09 19:45:16 -0300

    fix: Correção da Config para que não ocorra erro de CORS

--- frontend/src/services/api/axiosApi.ts
+++ frontend/src/services/api/axiosApi.ts
@@ -2,13 +2,7 @@ import axios, { type AxiosInstance } from 'axios';
 
 const config = {
   baseURL: import.meta.env.VITE_API_URL,
-  headers: {
-    'Access-Control-Allow-Credentials': 'true',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers':
-      'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json,multipart/form-data',
-    'Access-Control-Allow-Methods': 'GET,PUT,PATCH,POST,DELETE,OPTIONS',
-  },
+  
 };
 
 export default (path: string, headers?: any): AxiosInstance =>
