d231b9f4f2c44a490badeda02fd877a347615ca5
Caio Nunes <caionunes.profissional@gmail.com>
2025-06-20 16:40:20 -0300

    feat: Criação de componentes

---
+++ frontend/src/components/menu/MyToolbar.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Menu from '@/components/menu/MyMenu.vue';
+import Toolbar from 'primevue/toolbar';
+</script>
+
+<template>
+    <Toolbar class="mb-4 !bg-[#10b9816b] shadow-md !border-none h-15 ">
+        <template #start>
+            <Menu />
+        </template>
+    </Toolbar>
+</template>
